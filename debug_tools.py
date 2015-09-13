0148b2d36e98ad508cbb49fa481200df31fca594
Shiochi Sakane <sakane@tanu.org>
2015-09-13 23:11:53 +0900

    added debug_tools.py to print debugging message.

--- debug_tools.py
+++ debug_tools.py
@@ -1,1 +1,17 @@
-/Users/sakane/work/lang/python/debug_tools.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+def is_debug(level, west):
+    if west.jc.has_key('debug_level'):
+        if west.jc['debug_level'] >= level:
+            return True
+    return False
+
+import threading
+
+def debug_print(self, *args):
+    class_name = repr(self.__class__).split()[1]
+    thread_name = threading.currentThread().getName()
+    print('DEBUG:', class_name, thread_name, args)
